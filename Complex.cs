b8c9a9d81f80e8055e62c9bac43ab33855844012
davidpitutiu <56295878+davidpitutiu@users.noreply.github.com>
2020-04-30 16:32:49 +0300

    Add project files.

---
+++ Complex.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace Complex_Numbers
+{
+    public class Complex
+    {
+        Random rnd = new Random();
+        public double real { get; set; }
+        public double imaginary { get; set; }
+        public Complex()
+        {
+            this.real = rnd.Next(1, 20);
+            this.imaginary = rnd.Next(1, 20);
+        }
+        public Complex(double a, double b)
+        {
+            this.real = a;
+            this.imaginary = b;
+        }
+        public Complex(double a)
+        {
+            this.real = a;
+            this.imaginary= rnd.Next(1, 20); ;
+        }
+        public static void Sum(double real1, double real2, double imaginary1, double imaginary2)
+        {
+            double sumr;
+            double sumi;
+            sumr = real1 + real2;
+            sumi = imaginary1 + imaginary2;
+            Console.WriteLine($"z1+z2={sumr}+{sumi}i");
+        }
+        public static void Substraction(double real1, double real2, double imaginary1, double imaginary2)
+        {
+            double subr;
+            double subi;
+            subr = real1 - real2;
+            subi = imaginary1 - imaginary2;
+            Console.WriteLine($"z1-z2={subr}-{subi}i");
+
+        }
+        public static void Multiply(double real1, double real2, double imaginary1, double imaginary2)
+        {
+            double multr;
+            double multi;
+            multr = real1 * real2 - imaginary1 * imaginary2;
+            multi = real1 * imaginary2 + real2 * imaginary1;
+            Console.WriteLine($"z1*z2={multr}+{multi}i");
+        }
+       public static void Power(string c,double real,double imaginary,double n)
+        {
+            if (n == 1)
+            {
+                Console.WriteLine($"{c}={real}+{imaginary}i");
+            }
+            else
+            {
+                double real1 = real;
+                double imaginary1 = imaginary;
+                for (int i = 0; i < n; i++)
+                {
+                    real = real * real1 - imaginary * imaginary1;
+                    imaginary = real * imaginary1 + real1 * imaginary;
+                }
+                Console.WriteLine($"{c}^{n}={real}+{imaginary}i");
+            }
+        }
+        public static void PolarForm(string c,double real,double imaginary)
+        {
+            double r;
+            r = Math.Sqrt(Math.Pow(real, 2) + Math.Pow(imaginary, 2));
+            r = Math.Abs(r);
+            double t;
+            t = Math.Atan(imaginary / real);
+            Console.WriteLine($"The polar form of {c}=({r*Math.Cos(t)}+{r*Math.Sin(t)}i)");
+        }
+        public static void PolarFormPower(string c, double real, double imaginary,double n)
+        {
+            double r;
+            r = Math.Sqrt(Math.Pow(real, 2) + Math.Pow(imaginary, 2));
+            r = Math.Abs(r);
+            double t;
+            t = Math.Atan(imaginary / real);
+            Console.WriteLine($"The polar form of {c}^{n}={Math.Pow(r, n)*Math.Cos(t*n)}+{Math.Pow(r, n)*Math.Sin(t*n)}i");
+
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace Complex_Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Complex z1 = new Complex();
+            Console.WriteLine($"z1={z1.real}+{z1.imaginary}i");
+            Complex z2= new Complex();
+            Console.WriteLine($"z2={z2.real}+{z2.imaginary}i");
+            Complex.Sum(z1.real, z2.real, z1.imaginary, z2.imaginary);
+            Complex.Substraction(z1.real, z2.real, z1.imaginary, z2.imaginary);
+            Complex.Multiply(z1.real, z2.real, z1.imaginary, z2.imaginary);
+            Console.WriteLine("Enter the power of the complex number: ");
+            double n1 = double.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the complex number z1 or z2: ");
+            string c = Console.ReadLine();
+            bool d = true;
+            while(d==true)
+            {
+                if(c=="z1")
+                {
+                    Complex.Power(c,z1.real, z1.imaginary,n1);
+                    d = false;
+                }else if(c=="z2")
+                {
+                    Complex.Power(c,z2.real, z2.imaginary, n1);
+                    d = false;
+                }else
+                {
+                    Console.WriteLine("Try another character: ");
+                    c = Console.ReadLine();
+                }
+            }
+            Console.WriteLine("Enter the complex number z1 or z2 to find its polar form: ");
+            c = Console.ReadLine();
+            d = true;
+            while (d == true)
+            {
+                if (c == "z1")
+                {
+                    Complex.PolarForm(c, z1.real, z1.imaginary);
+                    d = false;
+                }
+                else if (c == "z2")
+                {
+                    Complex.PolarForm(c, z2.real, z2.imaginary);
+                    d = false;
+                }
+                else
+                {
+                    Console.WriteLine("Try another character: ");
+                    c = Console.ReadLine();
+                }
+
+            }
+            Console.WriteLine("Enter the n to find the polar form to the power of n of the complex number:");
+            double n2 = double.Parse(Console.ReadLine());
+            Console.WriteLine($"Enter the complex number z1 or z2 to find its polar form to the power of {n2}: ");
+            c = Console.ReadLine();
+            d = true;
+            while (d == true)
+            {
+                if (c == "z1")
+                {
+                    Complex.PolarFormPower(c, z1.real, z1.imaginary,n2);
+                    d = false;
+                }
+                else if (c == "z2")
+                {
+                    Complex.PolarFormPower(c, z2.real, z2.imaginary,n2);
+                    d = false;
+                }
+                else
+                {
+                    Console.WriteLine("Try another character: ");
+                    c = Console.ReadLine();
+                }
+
+            }
+        }
+    }
+}
